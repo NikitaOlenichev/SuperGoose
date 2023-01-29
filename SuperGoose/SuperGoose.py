2d0df16a1eb57972378610fde2e5fb2824a4ec6a
Nikita Olenichev <2020nikol@mail.ru>
2023-01-29 20:40:01 +0300

    started animation

--- SuperGoose/SuperGoose.py
+++ SuperGoose/SuperGoose.py
@@ -38,6 +38,29 @@ def load_image(name, colorkey=None):
     return image
 
 
+'''class AnimatedSprite(pygame.sprite.Sprite):
+    def __init__(self, sheet, columns, rows, x, y):
+        super().__init__(all_sprites)
+        self.frames = []
+        self.cut_sheet(sheet, columns, rows)
+        self.cur_frame = 0
+        self.image = self.frames[self.cur_frame]
+        self.rect = self.rect.move(x, y)
+
+    def cut_sheet(self, sheet, columns, rows):
+        self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
+                                sheet.get_height() // rows)
+        for j in range(rows):
+            for i in range(columns):
+                frame_location = (self.rect.w * i, self.rect.h * j)
+                self.frames.append(sheet.subsurface(pygame.Rect(
+                    frame_location, self.rect.size)))
+
+    def update(self):
+        self.cur_frame = (self.cur_frame + 1) % len(self.frames)
+        self.image = self.frames[self.cur_frame]'''
+
+
 tile_images = {
     'empty': pygame.transform.scale(load_image('ground.png'), (50, 50)),
     'wall': pygame.transform.scale(load_image('wall.png'), (50, 50))
@@ -56,9 +79,11 @@ def back():
 
 
 def start_screen():
+    #clock = pygame.time.Clock()
     screen = pygame.display.set_mode((500, 500))
-    rules = ["      SuperGoosе", ""]
     screen.fill((0, 100, 255))
+    #effects = AnimatedSprite(load_image('8.png'), 1, 4, 10, 10)
+    rules = ["      SuperGoosе", ""]
     font = pygame.font.SysFont('Times New Roman', 63)
     text_coord = 50
     Button(60, 200, 400, 75, 'Уровни', levels)
